24d5e88a2a4504b89343d6b8ab613bfae86fc949
tanatswanashe-zuze <tanatswanashe.zuze@student.moringaschool.com>
2022-03-13 22:28:25 +0200

    fixed indetation and javascript now working

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
 $(document).ready(function() {
     $(".design").click(function() {
         $("#designt").toggle();
+        $(".design").hide(function() {
+
+        });
+    });
+});
+$(document).ready(function() {
+    $("#designt").click(function() {
+        $(".design").toggle();
+        $("#designt").hide(function() {
+
+        });
     });
 });
 $(document).ready(function() {
     $(".dev").click(function() {
         $("#devt").toggle();
+        $(".dev").hide(function() {});
+    });
+});
+$(document).ready(function() {
+    $("#devt").click(function() {
+        $(".dev").toggle();
+        $("#devt").hide(function() {
+
+        });
     });
 });
 $(document).ready(function() {
     $(".file").click(function() {
         $("#filet").toggle();
+        $(".file").hide(function() {
+
+        });
+    });
+});
+$(document).ready(function() {
+    $("#filet").click(function() {
+        $(".file").toggle();
+        $("#filet").hide(function() {
+
+        });
     });
 });
