035223ec48172ab6b73db24a9d6ef421382c946e
tanatswanashe-zuze <tanatswanashe.zuze@student.moringaschool.com>
2022-03-13 18:16:06 +0200

    js toggle hide and show working

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".design").click(function() {
+        $("#designt").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".dev").click(function() {
+        $("#devt").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".file").click(function() {
+        $("#filet").toggle();
+    });
+});
